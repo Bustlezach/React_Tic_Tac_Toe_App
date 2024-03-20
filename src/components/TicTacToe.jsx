34277b74aca4236ba898fc95ba1f5e2152b2ecc0
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-03-20 15:11:31 +0100

    winningCombinations

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Board from './Board';
 
 const PLAYER_X = 'X';
 const PLAYER_O = 'O';
+
+const winningCombinations = [
+  {combo: [0, 1, 2], strikeClass: 'strike-row-1'},
+  {combo: [3, 4, 5], strikeClass: 'strike-row-2'},
+  {combo: [6, 7, 8], strikeClass: 'strike-row-3'},
+  {combo: [0, 3, 6], strikeClass: 'strike-column-1'},
+  {combo: [1, 4, 7], strikeClass: 'strike-column-2'},
+  {combo: [2, 5, 8], strikeClass: 'strike-column-3'},
+  {combo: [0, 4, 8], strikeClass: 'strike-diagonal-1'},
+  {combo: [2, 4, 6], strikeClass: 'strike-diagonal-2'},
+];
 
 function TicTacToe() {
   const [tiles, setTiles] = useState(new Array(9).fill(null));
@@ -23,6 +34,15 @@ function TicTacToe() {
       return currentPlayer;
     });
   };
+
+  const checkWinner = () => {
+    console.log('Check Winner');
+  };
+
+  useEffect(() => {
+    checkWinner();
+  }, [tiles]);
+
   return (
     <div>
       <h1>Tic Tac Toe</h1>
